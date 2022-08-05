a4781a80a26ca975a518a29fe73c3f22a6d13e5a
Martindohko <martin.viera.82@gmail.com>
2022-08-04 21:56:47 -0300

    Create 05-Tipos-Datos-Complejos-Objetos.js
    Definición de Objetos

---
+++ 05-Tipos-Datos-Complejos-Objetos.js
@@ -0,0 +1,25 @@
+/*Es un tipo de dato que intenta representar la realidad
+Pueden tener propiedades
+
+Objeto:
+    Propiedades:
+        -Color:blanco
+        -Forma: rectangular
+    Métodos
+        -Poner sombrero
+        -Quitar brazo
+       
+class Caja{
+    String Color = "blanco";
+    String Forma = "rectangular";
+    int Largo = 20;
+}
+
+class Juguete{
+    String Color = "marron";
+    String Forma = "papa";
+    
+    ponerSombrero();
+    quitarBrazo();
+}
+ */
